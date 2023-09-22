72bece25cf551621777115f7ee352e8429539c22
Eric Eastwood <contact@ericeastwood.com>
2023-09-22 05:00:57 -0500

    Implement X Input extension select_events

--- xinputext.zig
+++ xinputext.zig
@@ -31,6 +31,7 @@ pub const ExtOpcode = enum(u8) {
     get_device_key_mapping = 24,
     change_device_key_mapping = 25,
     get_device_modifier_mapping = 26,
+    select_events = 46,
     change_property = 57,
     get_property = 59,
 };
@@ -331,3 +332,99 @@ pub const ListInputDevicesReply = extern struct {
     }
 };
 comptime { std.debug.assert(@sizeOf(ListInputDevicesReply) == 32); }
+
+pub const event = struct {
+    pub const device_changed: u32 = (1 << 1);
+    pub const key_press: u32 = (1 << 2);
+    pub const key_release: u32 = (1 << 3);
+    pub const button_press: u32 = (1 << 4);
+    pub const button_release: u32 = (1 << 5);
+    pub const motion: u32 = (1 << 6);
+    pub const enter: u32 = (1 << 7);
+    pub const leave: u32 = (1 << 8);
+    pub const focus_in: u32 = (1 << 9);
+    pub const focus_out: u32 = (1 << 10);
+    pub const hierarchy: u32 = (1 << 11);
+    pub const property: u32 = (1 << 12);
+    // Events (v2.1)
+    pub const raw_key_press: u32 = (1 << 13);
+    pub const raw_key_release: u32 = (1 << 14);
+    pub const raw_button_press: u32 = (1 << 15);
+    pub const raw_button_release: u32 = (1 << 16);
+    pub const raw_motion: u32 = (1 << 17);
+    // Events (v2.2)
+    pub const touch_begin: u32 = (1 << 18);
+    pub const touch_update: u32 = (1 << 19);
+    pub const touch_end: u32 = (1 << 20);
+    pub const touch_ownership: u32 = (1 << 21);
+    pub const raw_touch_begin: u32 = (1 << 22);
+    pub const raw_touch_update: u32 = (1 << 23);
+    pub const raw_touch_end: u32 = (1 << 24);
+    // Events (v2.3)
+    pub const barrier_hit: u32 = (1 << 25);
+    pub const barrier_leave: u32 = (1 << 26);
+};
+
+pub const Device = enum(u16) {
+    all = 0,
+    all_master = 1,
+};
+
+pub const EventMask = struct {
+    device_id: x.NonExhaustive(Device),
+    mask: u32,
+};
+
+pub const select_events = struct {
+    const size_of_event_mask_over_the_wire =
+          2 // device_id
+        + 2 // mask_len
+        + 4 // mask
+    ;
+    comptime { std.debug.assert(size_of_event_mask_over_the_wire == 8); }
+
+    pub const non_option_len =
+              2 // extension and command opcodes
+              + 2 // request length
+              + 4 // window_id
+              + 2 // num_mask
+              + 2 // padding
+    ;
+    comptime { std.debug.assert(non_option_len == 12); }
+
+    pub fn getLen(num_masks: u16) u16 {
+        return non_option_len +
+            (size_of_event_mask_over_the_wire * num_masks);
+    }
+
+    pub const Args = struct {
+        window_id: u32,
+        masks: []EventMask,
+    };
+    pub fn serialize(buf: [*]u8, ext_opcode: u8, args: Args) u16 {
+        buf[0] = ext_opcode;
+        buf[1] = @intFromEnum(ExtOpcode.select_events);
+        const calculated_length = getLen(@as(u16, @intCast(args.masks.len)));
+        x.writeIntNative(u16, buf + 2, calculated_length >> 2);
+        x.writeIntNative(u32, buf + 4, args.window_id);
+        x.writeIntNative(u16, buf + 8, @as(u16, @intCast(args.masks.len)));
+        // 2 bytes of padding
+
+        var current_request_len: u16 = non_option_len;
+
+        for (args.masks) |mask| {
+            x.writeIntNative(u16, buf + current_request_len, @intFromEnum(mask.device_id));
+            // Length of mask in 4 byte units. Since our masks are always u32, this is always 1.
+            const mask_len: u16 = @sizeOf(u32) / 4;
+            x.writeIntNative(u16, buf + current_request_len + 2, mask_len);
+            x.writeIntNative(u32, buf + current_request_len + 4, mask.mask);
+
+            current_request_len += size_of_event_mask_over_the_wire;
+        }
+
+        // Quick sanity check that are assembled length is the same as the length we calculated.
+        std.debug.assert(current_request_len == calculated_length);
+
+        return current_request_len;
+    }
+};
